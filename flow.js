612fc93e262853c4de764ecc3797392a46092c7d
Roman Storm <rstormsf@gmail.com>
2017-10-19 02:24:44 -0700

    deploy

--- flow.js
+++ flow.js
@@ -4,12 +4,22 @@ const firebaseSession = require('telegraf-session-firebase')
 const admin = require('firebase-admin')
 const debug = require('debug')('app:app')
 const { Extra, Markup } = require('telegraf')
+require("dotenv-json")();
 
-
-const serviceAccount = require('./presalesyndicator-firebase-adminsdk-k84po-fd5442124f.json');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://presalesyndicator.firebaseio.com'
+    credential: admin.credential.cert({
+        type: process.env.type,
+        project_id: process.env.project_id,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url
+    }),
+    databaseURL: process.env.databaseURL
 })
 const database = admin.database()
 
@@ -21,8 +31,8 @@ async function isAdmin(from) {
     return admins.val();
 }
 
-async function login(from){
-    database.ref(`sessions/logins/${from.username}/`).set({...from, lastTime: Date.now()});
+async function login(from) {
+    database.ref(`sessions/logins/${from.username}/`).set({ ...from, lastTime: Date.now() });
 }
 app.command('start', async ({ session, reply, from }) => {
     console.log(from);
