f8e0a683bb2f5ab94812df941c0efc3f5f941f08
Roman Storm <rstormsf@gmail.com>
2017-10-19 01:47:19 -0700

    fix add ICO

--- flow.js
+++ flow.js
@@ -2,61 +2,84 @@ const Telegraf = require('telegraf')
 const TelegrafFlow = require('telegraf-flow')
 const firebaseSession = require('telegraf-session-firebase')
 const admin = require('firebase-admin')
-const LocalSession = require('telegraf-session-local')
 const debug = require('debug')('app:app')
 const { Extra, Markup } = require('telegraf')
 
 
-// const serviceAccount = require('./presalesyndicator-firebase-adminsdk-k84po-fd5442124f.json');
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: 'https://presalesyndicator.firebaseio.com'
-// })
-// const database = admin.database()
-
+const serviceAccount = require('./presalesyndicator-firebase-adminsdk-k84po-fd5442124f.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://presalesyndicator.firebaseio.com'
+})
+const database = admin.database()
 
 const app = new Telegraf(process.env.BOT_TOKEN)
-app.command('start', ({ reply }) => {
-    return reply('Hi there, please choose an option', Markup
-      .keyboard([
-        ['🔍 ICO Deals', '😎 Almost Closed <1hr'], // Row1 with 2 buttons
-        ['☸ Participate', '⭐️ My balance','📢 Check Tx'], // Row2 with 2 buttons
-        ['📢 My syndicates',  '/manageMembers', '👥 Add ICO'] // Row3 with 3 buttons
-      ])
-      .oneTime()
-      .resize()
-      .extra()
-    )
-    })
+app.use(firebaseSession(database.ref('sessions')))
 
-app.use((ctx, next) => {
-    const session = new LocalSession({ database: `sessions/${ctx.from.username}.json` }).middleware();
-    return session(ctx, next);
+async function isAdmin(from) {
+    const admins = await database.ref(`sessions/admins/${from}/status`).once('value');
+    return admins.val();
+}
+app.command('start', async ({ session, reply, from }) => {
+    console.log(from);
+    let isUserAdmin = await isAdmin(from.username);
+    return reply('Hi there, please choose an option', Markup
+        .keyboard([
+            ['🔍 ICO Deals', '😎 Almost Closed <1hr'], // Row1 with 2 buttons
+            ['☸ Participate', '⭐️ My balance', '📢 Check Tx'], // Row2 with 2 buttons
+            isUserAdmin ? ['📢 My syndicates', '/manageMembers', '👥 Add ICO'] : []
+        ])
+        .oneTime()
+        .resize()
+        .extra()
+    )
 })
+
 const createICO = require('./wizards/createICO');
+const newS = require('./wizards/newS');
 const ico_deals = require('./wizards/ico_deals');
 const manage_members = require('./wizards/manage_members');
 const { WizardScene, enter } = TelegrafFlow
-const flow = new TelegrafFlow()
+const flow = new TelegrafFlow();
 flow.register(createICO)
-flow.register(ico_deals)
-flow.register(manage_members)
-// app.use(firebaseSession(database.ref('sessions')))
+// flow.register(ico_deals)
+// flow.register(manage_members)
+// flow.register(newS)
+
 
 
 app.use(flow.middleware())
-app.on('text', (ctx, next) => {
-    ctx.session.counter = ctx.session.counter || 0
-    ctx.session.counter++
-    return next()
-})
-app.hears('/stats', ({ reply, session, from }) => reply(`${session.counter} messages from ${from.username}`))
+app.hears('/stats', ({ reply, session, from }) => reply(`${session.icos} messages from ${from.username}`))
 
-app.hears('👥 Add ICO', enter('create-ico'))
+app.hears('👥 Add ICO', ({ session, flow }) => {
+    console.log(session);
+    if (session._flow) {
+        session._flow._state = null;
+    }
+    if (!session.icos) {
+        session.icos = { "INITIALIZED": true }
+    }
+    flow.enter('create-ico')
+})
 app.hears('🔍 ICO Deals', enter('ico-deals'))
-app.hears('🤳 Manage Members', ({reply}) => {
+app.hears('🤳 Manage Members', ({ reply }) => {
     reply('/manageMembers')
 })
+// bot.hears('/stats', ({ reply, session, from }) => reply(`${session.counter} messages from ${from.username}`))
 
 app.command('/manageMembers', enter('manage-members'))
+app.command('/test', (ctx) => {
+    ctx.reply('sdf',
+        Extra.HTML().markup((m) => {
+            console.log(m);
+            // return m.button('sdfsd');
+            return m.inlineKeyboard([
+                // m.button('sdfsdf'),
+                m.urlButton('❤️', 'http://telegraf.js.org'),
+                m.callbackButton('Yes', 'Yes'),
+                m.callbackButton('No', 'No'),
+            ])
+        }
+        ))
+})
 app.startPolling()
--- wizards/createICO.js
+++ wizards/createICO.js
@@ -1,26 +1,32 @@
 const TelegrafFlow = require('telegraf-flow')
 const { WizardScene, enter } = TelegrafFlow;
 const { Extra, Markup } = require('telegraf')
-
-let icoCount = 0;
+const moment = require('moment')
 const createICO = new WizardScene('create-ico',
+// STEP 0
 (ctx) => {
-    if(ctx.session.icos && ctx.session.icos.length){
-        icoCount = ctx.session.icos.length;
-    } else {
-        ctx.session.icos = [];
-    }
-    ctx.session.icos.push({});
-    ctx.reply('Lets create an ICO, Please provide the name for your ICO:');
+    ctx.replyWithMarkdown('Lets create an ICO, Please provide the name for your ICO:');
     ctx.flow.wizard.next()
 },
+// STEP 1
 (ctx) => {
-    if (ctx.message && ctx.message.text.length === 1) {
+    let icoName
+    if(ctx.message){
+         icoName = ctx.message.text;
+    }
+    if (ctx.message && icoName.length === 1) {
         ctx.flow.wizard.selectStep(0);
         return ctx.flow.reenter('create-ico');
     }
-    ctx.session.icos[icoCount].icoName = ctx.message.text;
-    // ctx.reply('Please provide a currency in which the ICO will be held?')
+    ctx.flow.state.icoName = icoName;
+    if(ctx.session.icos[icoName] ) {
+        ctx.replyWithMarkdown(`*Name already exists*
+        *${icoName}*
+        `)
+        ctx.flow.wizard.selectStep(0);
+        return ctx.flow.reenter('create-ico');
+    }
+    
     ctx.reply('Please choose a currency in which the ICO will be contributed <b>ETH</b> or <i>BTC</i>', Extra.HTML().markup((m) =>
     m.inlineKeyboard([
       m.callbackButton('ETH', 'ETH'),
@@ -28,46 +34,85 @@ const createICO = new WizardScene('create-ico',
     ])))
     ctx.flow.wizard.next()
 },
+// STEP 2
 (ctx) => {
-    if (ctx.message && ctx.message.text.length > 0) {
+    if (!ctx.flow.state.currency && ctx.message && ctx.message.text.length > 0) {
         ctx.flow.wizard.selectStep(1);
         return ctx.flow.reenter('create-ico');
+    } else {
+        if(!ctx.flow.state.currency){
+            ctx.flow.state.currency = ctx.callbackQuery.data;
+        }
+        ctx.reply(`Max Cap in ${ctx.flow.state.currency}`)
+        ctx.flow.wizard.next()
     }
-    ctx.session.icos[icoCount].currency = ctx.callbackQuery.data;
-    ctx.reply(`Max Cap in ${ctx.callbackQuery.data}`)
-    ctx.flow.wizard.next()
 },
+// STEP 3
 (ctx) => {
-    if (ctx.message && !Number.isInteger(Number(ctx.message.text))) {
+    if (!ctx.flow.state.maxCap && ctx.message && !Number.isInteger(Number(ctx.message.text))) {
         ctx.flow.wizard.selectStep(2);
         return ctx.flow.reenter('create-ico');
-    }
-    ctx.session.icos[icoCount].maxCap = ctx.message.text;
-    ctx.reply(`Time Starts in following format month/day/year hour:minute UTC. Example: 12/31/2017 15:31`)
-    ctx.flow.wizard.next()
-},
-(ctx) => {
-    ctx.session.icos[icoCount].startTime = ctx.message.text;
-    ctx.reply(`Time ENDs following format month/day/year hour:minute UTC. Example: 12/31/2017 15:31`)
-    ctx.flow.wizard.next()
-},
-(ctx) => {
-    ctx.session.icos[icoCount].endTime = ctx.message.text;
-    ctx.reply(`Confirm? ${JSON.stringify(ctx.session.icos[icoCount], null, '\n')}`, Extra.HTML().markup((m) =>
-    m.inlineKeyboard([
-      m.callbackButton('Yes', 'Yes'),
-      m.callbackButton('No', 'No')
-    ])))
-    ctx.flow.wizard.next()
-},
-(ctx) => {
-    if(ctx.callbackQuery.data === 'Yes'){
-        ctx.reply(`Here is your ICO info: ${JSON.stringify(ctx.session.icos[icoCount])}`)
-        ctx.flow.leave()
     } else {
-        ctx.reply(`Your ICO was not created: ${JSON.stringify(ctx.session.icos[icoCount])}`)
-        ctx.session.icos.pop();
-        ctx.flow.leave()
+        if(!ctx.flow.state.maxCap){
+            ctx.flow.state.maxCap = ctx.message.text;
+        }
+        ctx.reply(`Start Date and Time in following format month/day/year hour:minute UTC time zone.\n Example: 12/31/2017 15:31`)
+        ctx.flow.wizard.next()
+    }
+},
+// STEP 4
+(ctx) => {
+    if(!ctx.flow.state.startTime){
+        let startTime = moment.utc(ctx.message.text, "MM-DD-YYYY HH:mm")
+        if(startTime.toString() !== 'Invalid date'){
+            ctx.flow.state.startTime = startTime.format();
+        }
+    }
+    if(!ctx.flow.state.startTime){
+        ctx.flow.wizard.selectStep(3);
+        return ctx.flow.reenter('create-ico');
+    } else {
+        ctx.reply(`End Date and Time in the following format month/day/year hour:minute UTC time zone.\n Example: 12/31/2017 15:31`)
+        ctx.flow.wizard.next()
+    }
+},
+// STEP 5
+(ctx) => {
+    if(!ctx.flow.state.endTime){
+        let endTime = moment.utc(ctx.message.text, "MM-DD-YYYY HH:mm")
+        if(endTime.toString() !== 'Invalid date' && endTime.isAfter(ctx.flow.state.startTime)) {
+            ctx.flow.state.endTime = endTime.format();
+        }
+    }
+    if(!ctx.flow.state.endTime){
+        ctx.flow.wizard.selectStep(4);
+        return ctx.flow.reenter('create-ico');
+    } else {
+        ctx.reply(`Please Confirm:
+            Name: <b>${ctx.flow.state.icoName}</b>
+            Max Cap <b>${ctx.flow.state.maxCap} ${ctx.flow.state.currency}</b>
+            Start <b>${moment.utc(ctx.flow.state.startTime).format("dddd, MMMM Do YYYY, h:mm:ss a")} GMT</b>
+            End<b> ${moment.utc(ctx.flow.state.endTime).format("dddd, MMMM Do YYYY, h:mm:ss a")} GMT</b>
+            Currency <b>${ctx.flow.state.currency}</b>
+            `, Extra.HTML().markup((m) =>
+            m.inlineKeyboard([
+            m.callbackButton('Yes', 'Yes'),
+            m.callbackButton('No', 'No'),
+        ])))
+        ctx.flow.wizard.next()
+    }
+},
+(ctx) => {
+    if(ctx.callbackQuery && ctx.callbackQuery.data === 'Yes'){
+        const {icoName, maxCap, startTime, endTime, currency} = ctx.flow.state;
+        ctx.session.icos[icoName] = {
+            maxCap, startTime, endTime, currency
+        }
+        ctx.reply(`ICO saved\n /start`)
+        ctx.session._flow = null;
+    } else {
+        ctx.reply(`Your ICO was not created`)
+        ctx.session._flow = null;
     }
 },
 )
