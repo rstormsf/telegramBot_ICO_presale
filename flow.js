9f2fb6a78c9c3c16f1598983b80a6943f56dc73c
Roman Storm <rstormsf@gmail.com>
2017-10-19 15:32:05 -0700

    add auth

--- flow.js
+++ flow.js
@@ -1,27 +1,10 @@
 const Telegraf = require('telegraf')
+const {Telegram} = require('telegraf')
 const TelegrafFlow = require('telegraf-flow')
 const firebaseSession = require('telegraf-session-firebase')
-const admin = require('firebase-admin')
+const database = require('./firebase.config');
 const debug = require('debug')('app:app')
 const { Extra, Markup } = require('telegraf')
-require('dotenv').config();
-console.log(process.env.private_key)
-admin.initializeApp({
-    credential: admin.credential.cert({
-        type: process.env.type,
-        project_id: process.env.project_id,
-        private_key_id: process.env.private_key_id,
-        private_key: process.env.private_key,
-        client_email: process.env.client_email,
-        client_id: process.env.client_id,
-        auth_uri: process.env.auth_uri,
-        token_uri: process.env.token_uri,
-        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-        client_x509_cert_url: process.env.client_x509_cert_url
-    }),
-    databaseURL: process.env.databaseURL
-})
-const database = admin.database()
 
 const app = new Telegraf(process.env.BOT_TOKEN)
 app.use(firebaseSession(database.ref('sessions')))
@@ -37,12 +20,13 @@ async function login(from) {
 app.command('start', async ({ session, reply, from }) => {
     session._flow = null;
     let isUserAdmin = await isAdmin(from.username);
+
     login(from);
     return reply('Hi there, please choose an option', Markup
         .keyboard([
             ['🔍 ICO Deals', '😎 Almost Closed <1hr'], // Row1 with 2 buttons
             ['☸ Participate', '⭐️ My balance', '📢 Check Tx'], // Row2 with 2 buttons
-            isUserAdmin ? ['📢 My syndicates', '/manageMembers', '👥 Add ICO'] : []
+            isUserAdmin ? ['📢 My syndicates', '🤳 Manage Members', '👥 Add ICO'] : []
         ])
         .oneTime()
         .resize()
@@ -51,28 +35,44 @@ app.command('start', async ({ session, reply, from }) => {
 })
 
 const createICO = require('./wizards/createICO');
+const manageMembers = require('./wizards/manage_members');
 
 const { WizardScene, enter } = TelegrafFlow
 const flow = new TelegrafFlow();
 flow.register(createICO)
+flow.register(manageMembers)
 
 
 
 
 app.use(flow.middleware())
 
-app.hears('👥 Add ICO', ({ session, flow }) => {
-    console.log(session);
+app.hears('👥 Add ICO', async ({ session, flow, from, reply }) => {
     session._flow = null;
-    if (!session.icos) {
-        session.icos = { "INITIALIZED": true }
+    let isUserAdmin = await isAdmin(from.username);
+    if(isUserAdmin){
+        if (!session.icos) {
+            session.icos = { "INITIALIZED": true }
+        }
+        flow.enter('create-ico')
+    } else{
+        reply('you are not authorized')
     }
-    flow.enter('create-ico')
 })
 // app.hears('🔍 ICO Deals', enter('ico-deals'))
-// app.hears('🤳 Manage Members', ({ reply }) => {
-//     reply('/manageMembers')
-// })
+app.hears('🤳 Manage Members', async ({ reply, from, flow }) => {
+    let isUserAdmin = await isAdmin(from.username);
+    if(isUserAdmin){
+        flow.enter('manage-members')
+    } else {
+        reply('you are not authorized')
+    }
+})
 
+let t = new Telegram(process.env.BOT_TOKEN)
+
+app.command('/addMember', (ctx) => {
+    console.log(ctx);
+  })
 // app.command('/manageMembers', enter('manage-members'))
 app.startPolling()
