536fec67e1d7f6b66eb89f3b8d67f6db81062efa
ryan <rleinbox@gmail.com>
2017-10-23 11:36:27 -0700

    got rid of ssl

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const bot = require('./src/bot');
 if (NODE_ENV === 'production') {
   bot.startWebhook(`/${ICOBOT_TOKEN}`, null, PORT);
 } else if (NODE_ENV === 'dev') {
-  const tlsOptions = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt'),
-  };
+  // const tlsOptions = {
+  //   key: fs.readFileSync('server.key'),
+  //   cert: fs.readFileSync('server.crt'),
+  // };
   console.log(ICOBOT_TOKEN);
   bot.startWebhook(`/${ICOBOT_TOKEN}`, null, 8443);
 }
