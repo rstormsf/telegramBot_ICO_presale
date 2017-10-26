048a1d956ff0a688ce7216fabb05e784666227e7
ryan <rleinbox@gmail.com>
2017-10-25 21:24:46 -0700

    .env.dev removed, contract deployment code added

--- app.js
+++ app.js
@@ -2,20 +2,9 @@ const fs = require('fs');
 const { NODE_ENV, PORT, ICOBOT_TOKEN } = require('./config');
 const bot = require('./src/bot');
 
-
-if (NODE_ENV === 'production') {
-  bot.startWebhook(`/${ICOBOT_TOKEN}`, null, PORT);
-} else if (NODE_ENV === 'dev') {
-  // const tlsOptions = {
-  //   key: fs.readFileSync('server.key'),
-  //   cert: fs.readFileSync('server.crt'),
-  // };
-  console.log(ICOBOT_TOKEN);
-  bot.startWebhook(`/${ICOBOT_TOKEN}`, null, 8443);
-}
+bot.startWebhook(`/${ICOBOT_TOKEN}`, null, PORT);
 
 console.log(`Server is listening to localhost:${PORT}`);
-
 
 process.on('unhandledRejection', (e) => {
   console.log(e);
--- config.js
+++ config.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 
 
-if (process.env.NODE_ENV === 'production') {
-  dotenv.config();
-} else if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
   process.env = dotenv.parse(fs.readFileSync('.env.dev'));
 }
 
 module.exports = {
-  NODE_ENV: 'dev',
+  NODE_ENV: process.env.NODE_ENV,
   ICOBOT_TOKEN: process.env.ICOBOT_TOKEN,
   PORT: process.env.PORT,
   FIREBASE_URL: process.env.FIREBASE_URL,
+  PRIVATE_KEY: process.env.PRIVATE_KEY,
 };
