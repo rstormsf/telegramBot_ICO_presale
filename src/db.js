dab492068e8a0f3a4f98698b3b55f4faca57f80f
Roman Storm <rstormsf@gmail.com>
2017-10-28 19:45:10 -0700

    fix firebase

--- src/db.js
+++ src/db.js
@@ -1,12 +1,22 @@
 const config = require('../config');
 const admin = require('firebase-admin')
- 
+
+let private_key;
+private_key = process.env.private_key
+if(process.env.NODE_ENV === 'prod'){
+  private_key = '-----BEGIN PRIVATE KEY-----\n';
+  // -----BEGIN PRIVATE KEY-----\n
+  let key = process.env.private_key.split(' ').join('\n');
+  private_key = private_key + key + '\n-----END PRIVATE KEY-----\n';
+} else {
+  private_key = process.env.private_key;
+}
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.type,
     project_id: process.env.project_id,
     private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
+    private_key,
     client_email: process.env.client_email,
     client_id: process.env.client_id,
     auth_uri: process.env.auth_uri,
